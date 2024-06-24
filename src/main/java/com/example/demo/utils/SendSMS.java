0f995a0daa8f7f12aefa0f1ee29fcef190b2e7c5
Administrator <1900481662@qq.com>
2024-06-25 00:14:43 +0800

    1111
    Signed-off-by: Administrator <1900481662@qq.com>

--- src/main/java/com/example/demo/utils/SendSMS.java
+++ src/main/java/com/example/demo/utils/SendSMS.java
@@ -22,11 +22,13 @@ public class SendSMS {
     public static Client createClient() throws Exception {
         // 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考。
         // 建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378657.html。
+
+
         com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
                 // 必填，请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID。
-                .setAccessKeyId("LTAI5tGiJpQANegi7vN1LZGo")
+                .setAccessKeyId("====")
                 // 必填，请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
-                .setAccessKeySecret("GGrce47gYTpp7Wyb7q6fK20lMqoWsU");
+                .setAccessKeySecret("====");
         // Endpoint 请参考 https://api.aliyun.com/product/Dysmsapi
         config.endpoint = "dysmsapi.aliyuncs.com";
         return new Client(config);
